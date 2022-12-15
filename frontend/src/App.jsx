facec29cb85b3db932ccd93eef72225ce75371fd
itxSaaad <saadstudent.cs@gmail.com>
2022-12-15 13:16:58 +0500

    Setting Up: Front-End Using React.js

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Salah Tracker App</h1>
+      <p>Best App to Track your Salah!</p>
+    </div>
+  );
+}
+
+export default App;
