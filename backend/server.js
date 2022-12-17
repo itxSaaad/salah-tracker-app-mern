a6ed55333b953ba9387711613211297d080b9103
itxSaaad <saadstudent.cs@gmail.com>
2022-12-17 16:52:07 +0500

    Setting Up Backend USing Node.js & Express.js

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import express, { json } from "express";
+import { config } from "dotenv";
+import cors from "cors";
+import colors from "colors";
+import path from "path";
+
+const app = express();
+
+config();
+
+app.use(json());
+app.use(cors());
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
